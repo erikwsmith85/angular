bc75be63ce3b113933cf7f0bbd5be608a8c7862c
Erik Smith <erik.smith@mtsquanta.com>
2018-12-06 16:39:46 -0600

    data-binding

--- conFusion/src/app/shared/dish.ts
+++ conFusion/src/app/shared/dish.ts
@@ -1,3 +1,4 @@
+import {Comment} from './comment';
 export class Dish {
     id: string;
     name: string;
@@ -6,6 +7,7 @@ export class Dish {
     featured: boolean;
     label: string;
     price: string;
-    description: string
+    description: string;
+    comments: Comment [];
 
 }
