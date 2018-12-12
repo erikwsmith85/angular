a3abccec9562304907086877baef1734c084498d
Erik Smith <erik.smith@mtsquanta.com>
2018-12-12 17:05:44 -0600

    login form

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -7,7 +7,11 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {FlexLayoutModule} from '@angular/flex-layout';
-
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
 
 import 'hammerjs';
 
@@ -25,6 +29,7 @@ import {PromotionService} from './services/promotion.service';
 import {LeaderService} from './services/leader.service';
 
 import {AppRoutingModule} from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +40,8 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -46,7 +52,12 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule
 
   ],
   //services included in Providers[] are available globally
@@ -55,6 +66,11 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
         PromotionService,
         LeaderService
   ],
+
+  entryComponents: [
+    LoginComponent
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
